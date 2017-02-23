064a890d00db6b2a4670d835390c397f045ae045
Kevin <oui@non.peutetre>
2017-02-23 20:50:56 +0100

    ajout de l'index requests

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def run(filename):
         video_len, endpoint_len, request_len, cache_len, cache_size = next(_file).split()
         video_sizes = [int(size) for size in next(_file).split()]
         cache_servers = [int(cache_size) for _ in range(0, int(cache_len))]
+        requests = []
         endpoints = []
         for endpoint_id in range(0, int(endpoint_len)):
             latency, cache_server_len = next(_file).split()
@@ -20,8 +21,12 @@ def run(filename):
 
         for line in _file:
             video_nbr, endpoint_id, requests_len = line.split()
+            requests.append({"endpoint_id": int(endpoint_id),
+                    "video_id": int(video_nbr),
+                    "latency": int(int(requests_len) * endpoints[int(endpoint_id)]["dc_latency"])})
             endpoints[int(endpoint_id)]["videos"].append({"id": int(video_nbr),
                                                           "requests" : int(requests_len)})
+        print(requests)
 
 
 if __name__ == "__main__":
