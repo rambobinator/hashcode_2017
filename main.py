eb6403840e4419618fc13c779644d583eae5e783
rmasse <roman.masse@gmail.com>
2017-02-24 10:28:35 +0100

    Cast to int using map

--- main.py
+++ main.py
@@ -23,26 +23,26 @@ def main(video_sizes, cache_servers, endpoints):
  
 def run(filename):
     with open(filename) as _file:
-        video_len, endpoint_len, request_len, cache_len, cache_size = next(_file).split()
-        video_sizes = [int(size) for size in next(_file).split()]
-        cache_servers = [int(cache_size) for _ in range(0, int(cache_len))]
+        video_len, endpoint_len, request_len, cache_len, cache_size = map(int, next(_file).split())
+        video_sizes = map(int, next(_file).split())
+        cache_servers = [cache_size for _ in range(0, cache_len)]
         endpoints = []
-        for endpoint_id in range(0, int(endpoint_len)):
-            latency, cache_server_len = next(_file).split()
+        for endpoint_id in range(0, endpoint_len):
+            latency, cache_server_len = map(int, next(_file).split())
             endpoints.append({"id": endpoint_id,
-                              "dc_latency": int(latency),
+                              "dc_latency": latency,
                               "videos": [],
                               "servers_cache": []})
-            for _ in range(0, int(cache_server_len)):
-                cache_server_id, cache_server_latency = next(_file).split()
-                endpoints[endpoint_id]["servers_cache"].append({"id": int(cache_server_id),
-                                                                "latency": int(cache_server_latency)})
+            for _ in range(0, cache_server_len):
+                cache_server_id, cache_server_latency = map(int, next(_file).split())
+                endpoints[endpoint_id]["servers_cache"].append({"id": cache_server_id,
+                                                                "latency": cache_server_latency})
             endpoints[endpoint_id]["servers_cache"].sort(key=lambda k: k.get("id"))
 
         for line in _file:
-            video_nbr, endpoint_id, requests_len = line.split()
-            endpoints[int(endpoint_id)]["videos"].append({"id": int(video_nbr),
-                                                          "requests" : int(requests_len)})
+            video_nbr, endpoint_id, requests_len = map(int, line.split())
+            endpoints[endpoint_id]["videos"].append({"id": video_nbr,
+                                                          "requests": requests_len})
         main(video_sizes, cache_servers, endpoints)
 
 
